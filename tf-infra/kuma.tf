c8bf7d0176c133f4b55abd75a91b5c94a17ac322
Luka Klaric <luka@debian.local>
2024-10-25 18:23:57 +0200

    quick commit

--- tf-infra/kuma.tf
+++ tf-infra/kuma.tf
@@ -22,7 +22,6 @@ resource "kubernetes_service" "svc-kuma" {
   }
 }
 
-
 resource "kubernetes_ingress_v1" "ingress_kuma" {
   metadata {
     name = "kuma"
@@ -44,6 +43,67 @@ resource "kubernetes_ingress_v1" "ingress_kuma" {
         }
       }
       host = "kuma.env"
+    }
+  }
+}
+
+resource "kubernetes_stateful_set" "stateful_set_kuma" {
+  metadata {
+    name = "ing-kuma"
+    namespace = "kuma"
+  }
+  spec {
+    replicas = 1
+    service_name = "svc-kuma"
+    selector {
+      match_labels = {
+        app = "uptime-kuma"
+      }
+    }
+    template  {
+      metadata {
+        labels = {
+          app = "uptime-kuma"
+        }
+      }
+      spec {
+        container {
+          image = "louislam/uptime-kuma"
+          name = "uptime-kuma"
+          env {
+            name = "UPTIME_KUMA_PORT"
+            value = "3001"
+          }
+          env {
+            name = "PORT"
+            value = "3001"
+          }
+          port {
+            container_port = "3001"
+            name = "uptime-kuma"
+            protocol = "TCP"
+          }
+          volume_mount {
+            name = "kuma-data"
+            mount_path = "/app/data"
+          }
+        }
+        volume_claim_template {
+          metadata {
+            name = "kuma-data"
+          }
+          spec {
+            access_mode:
+              - ReadWriteOnce
+            volume_mode = "Filesystem"
+            resources {
+              requests = {
+                storage = "1Gi"
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
